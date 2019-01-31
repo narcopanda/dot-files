844cfc65675530e8f521767608913b6184a66a22
sean rowland <seanrowl@buffalo.edu>
2019-01-31 11:24:36 -0500

    update install

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # This is a collection of my dot-files from vim to i3
-This also has a script the install my config for vim and tmux
+This also has a script the install my config for vim, tmux, and you complete me
--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp .vimrc ~/
 cp .tmux.cong ~/
-cp .ycm_extra_conf.py
+cp .ycm_extra_conf.py ~/.vim/
