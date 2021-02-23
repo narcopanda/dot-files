3825886393732f459a972dd1ade231da3874746a
narcopanda <srowla94@gmail.com>
2021-02-23 10:14:20 -0500

    updated install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+sudo apt install build-essential cmake python3-dev
+wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
+sudo apt-get update
+sudo apt-add-repository "deb http://apt.llvm.org/bionic/ llvm-toolchain-bionic-6.0 main"
+sudo apt-get install -y clang-6.0 lld-6.0
+
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp .vimrc ~/
